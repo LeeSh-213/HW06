57eb7a24a295b09d33f1ec5cdad565f15284ca96
LeeSh-213 <hasanghyun66@naver.com>
2025-07-09 10:01:08 +0900

    기본 파일 커밋

---
+++ HW06/Source/HW06/Public/RotationActor.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BaseActor.h"
+#include "RotationActor.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HW06_API ARotationActor : public ABaseActor
+{
+	GENERATED_BODY()
+	
+};
